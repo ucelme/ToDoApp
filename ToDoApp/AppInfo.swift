5cbd778248cd77a69bcb1c5ab603fed6afa0668e
ucelme <ucelme@gmail.com>
2020-12-27 12:15:21 +0300

    Fixing.

--- ToDoApp/AppInfo.swift
+++ ToDoApp/AppInfo.swift
@@ -2,6 +2,6 @@ import Foundation
 
 struct AppInfo {
     static let appName = "To Do App"
-    static let appID = 1546432673
-    static let oneTimePurchase = "one_time"
+    static let appID = 1546484752
+    static let oneTimePurchase = "one_time_to_do"
 }
